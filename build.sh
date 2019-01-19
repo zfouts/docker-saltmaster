09418027043201af18d1246414abb841b7cae337
Zachary Fouts <zachary.fouts@gmail.com>
2019-01-19 08:30:47 +0000

    remove default variables

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 set -o pipefail
 set -e
-set -x
-
-# You need VERSON_IMAGE_TAG, IMAGE, REPO_NAME, EMAIL defined.
-VERSION_IMAGE_TAG=2018.3.3
-EMAIL=zach@linux.com
-REPO_NAME=fouts/salt-master
-IMAGE=ubuntu:16.04
 
 cmd=$1
+
 docker_build() {
     local build_sha=$(git rev-parse HEAD)
     docker build --build-arg IMAGE=${IMAGE} --build-arg EMAIL=${EMAIL} --label build.sha=${build_sha} -t ${REPO_NAME}:master .
